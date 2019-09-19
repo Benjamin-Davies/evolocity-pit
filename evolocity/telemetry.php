dcea93046d3d50b6bd0e6e00c60199cbae513bcb
Benjamin-Davies <bentendavies@gmail.com>
2019-09-19 12:58:05 +1200

    Fix cors and update the site

--- evolocity/telemetry.php
+++ evolocity/telemetry.php
@@ -1,25 +1,36 @@
 <?php
 require_once('./includes/connect.php');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+header("Access-Control-Allow-Origin: *");
 
 switch ($_SERVER['REQUEST_METHOD']) {
-//case 'GET':
-//  $query = $pdo->prepare('SELECT Username, LifeCount, Score FROM scoreboard ORDER BY Score DESC LIMIT 100');
-//  $query->execute();
-//  $scores = $query->fetchAll(PDO::FETCH_ASSOC);
-//
-//  header('Content-Type: application/json');
-//
-//  echo json_encode($scores);
-//  break;
+case 'GET':
+  $query = $pdo->prepare('SELECT * FROM telemetry');
+  $query->execute();
+  $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  header('Content-Type: application/json');
+
+  echo json_encode($data);
+  break;
 case 'POST':
+  $input = json_decode(file_get_contents('php://input'), true);
+
   $query = $pdo->prepare('
     INSERT INTO telemetry
-    VALUES (:time, :voltage, :current, :speed)');
-  $query->bindValue('time', $_POST['time']);
-  $query->bindValue('voltage', $_POST['voltage']);
-  $query->bindValue('current', $_POST['current']);
-  $query->bindValue('speed', $_POST['speed']);
-  $query->execute();
+    VALUES (:time, :voltage, :current, :speed, :battery_voltage, :location)');
+  foreach ($input as $key => $value) {
+    $query->bindValue($key, $value);
+  }
+  $res = $query->execute();
+  if (!$res) {
+    echo 'no result';
+  } else {
+    echo $res;
+  }
+  break;
+default:
+  http_response_code(405);
   break;
 }
 ?>
