683a860454266b9154911b77a7df3335c16cf58d
Benjamin-Davies <bentendavies@gmail.com>
2019-10-05 10:48:28 +1300

    Add some code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { useState, useEffect } from 'react';
+//import Chart from 'react-google-charts';
+
 import { getTelemetryData } from './telemetry';
+
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([{
+    date: new Date(),
+    battery_voltage: 0,
+    voltage: 0,
+    current: 0,
+    location: null,
+  }]);
+  const lastData = data[data.length - 1];
 
   useEffect(() => {
     getTelemetryData()
       .then(setData);
   }, []);
 
+  useEffect(() => {
+    if (data.length > 1) {
+      const timeout = setTimeout(() => {
+        getTelemetryData(lastData.time)
+          .then(res => {
+            setData([...data, ...res]);
+          });
+      }, 500);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [data, lastData, setData]);
+
+  console.log(lastData);
   return (
     <div className="App">
-      <header className="App-status">
+      <div className="App-status">
         <span className="left orange">&tau;-morrow</span>
+        <span className="right">{lastData.battery_voltage}V</span>
+        <span className="right">{lastData.current}A</span>
+      </div>
+      <header className="App-header">
+        <h1>Welcome to &tau;-morrow pit</h1>
       </header>
       <main className="App-main">
-        <h1>Welcome to &tau;-morrow pit</h1>
-        <ul>
-          { data.map(d => (<li key={d.time}>{new Date(d.time).toString()} - {d.battery_voltage}</li>)) }
-        </ul>
+    {/*<Chart
+          width={0.4 * window.innerWidth}
+          height={600}
+          chartType="Line"
+          loader={<div>Loading...</div>}
+          data={
+            [['Time', 'Voltage', 'Current', 'Speed', 'Battery']]
+              .concat(data.map(
+                ({time, voltage, current, speed, battery_voltage}) =>
+                  [time, voltage, current, speed, battery_voltage]))}
+          options={{
+            chart: {
+              title: 'Car Data',
+            },
+          }}
+          />*/}
       </main>
     </div>
   );
--- src/telemetry.js
+++ src/telemetry.js
@@ -1,4 +1,7 @@
-export async function getTelemetryData() {
-  const res = await fetch('https://php.mmc.school.nz/201BH/benjamindavies/evolocity/telemetry');
+const start = new Date();
+
+export async function getTelemetryData(since = new Date(start.getFullYear(), start.getMonth(), start.getDate(), 0, 0, 0)) {
+  const res = await fetch(
+    `https://php.mmc.school.nz/201BH/benjamindavies/evolocity/telemetry?since=${since}`);
   return await res.json();
 }
