b31de56182563958c3e46878a7e143c3c64f6bbb
Benjamin-Davies <bentendavies@gmail.com>
2019-10-24 12:08:43 +1300

    Switch to client-side firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBSeAWcNcpOhjivaOnpoCEyhFoIPxTw-L4",
+  authDomain: "tau-morrow.firebaseapp.com",
+  databaseURL: "https://tau-morrow.firebaseio.com",
+  projectId: "tau-morrow",
+  storageBucket: "tau-morrow.appspot.com",
+  messagingSenderId: "429800378148",
+  appId: "1:429800378148:web:26445bf438e92f6cdf2db1"
+};
--- src/telemetry.js
+++ src/telemetry.js
@@ -1,13 +1,21 @@
-const baseUrl = process.env.NODE_ENV === 'production'
-  ? ''
-  : 'http://localhost:5000/tau-morrow/us-central1';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+import firebaseConfig from './firebase-config';
+
+export const app = firebase.initializeApp(firebaseConfig);
+export const db = app.firestore();
+
+export const sensors = db.collection('sensors');
 
 /**
  * @param{Date} start
  * @param{Date} end
  */
 export async function getTelemetryData(start, end) {
-  const res = await fetch(
-    `${baseUrl}/api/sensors/${start.toISOString()}/${end.toISOString()}`);
-  return await res.json();
+  const query = sensors
+    .where('time', '>', start)
+    .where('time', '<', end);
+  const snapshot = await query.get();
+  return snapshot.docs.map(doc => doc.data());
 }
