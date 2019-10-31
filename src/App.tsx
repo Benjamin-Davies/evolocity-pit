a2b9a9d7f73d2262193442588146ced8ca3c7e42
Benjamin Davies <bentendavies@gmail.com>
2019-11-01 09:57:59 +1300

    Add DataSelector and redo aesthetics

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Map, TileLayer } from 'react-leaflet';
 import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 
-import { SensorData, getDataRange } from './telemetry';
+import { SensorData } from './telemetry';
+import DataSelector from './DataSelector';
 
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 
 function App() {
-  const [startTime, setStartTime] = useState(new Date(2019, 8, 22));
-  const [endTime, setEndTime] = useState(new Date());
-
   const [data, setData] = useState<SensorData[]>([]);
   const lastData = data[data.length - 1];
 
@@ -27,67 +25,53 @@ function App() {
       // Fallback to zoomed-out view of tauranga
       : [[-37.69, 176.17], 15];
 
-  useEffect(() => {
-    getDataRange(startTime, endTime)
-      .then(setData);
-    //const sub = getDataStream()
-    //  .subscribe(newData => {
-    //   setData([...data, newData]);
-    //  });
-    //return () => sub.unsubscribe();
-  }, [startTime, endTime]);
-
-  console.log(data);
   return (
     <div className="App">
       <div className="App-status">
         <span className="left orange">&tau;-morrow</span>
-        { lastData ? (
+        {lastData ? (
           <>
             <span className="right">{battery_voltage.toFixed(2)}V</span>
             <span className="right">{current.toFixed(2)}A</span>
           </>
-          ) : (
-            <span className="right">{2*Math.PI}</span>
-          ) }
+        ) : (
+          <span className="right">{2 * Math.PI}</span>
+        )}
       </div>
       <header className="App-header">
         <h1>Welcome to &tau;-morrow pit</h1>
       </header>
       <main className="App-main">
-        <input type="datetime-local"
-          value={startTime.toISOString().slice(0, -1)}
-          onChange={ev => setStartTime(new Date(ev.target.value))} />
-        <input type="datetime-local"
-          value={endTime.toISOString().slice(0, -1)}
-          onChange={ev => setEndTime(new Date(ev.target.value))} />
-        { lastData ? (
+        <DataSelector setData={setData} />
+        {lastData ? (
           <div className="App-row">
             <Map center={center} zoom={zoom}>
               <TileLayer
                 attribution="&amp;copy <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
             </Map>
             <div className="chart-container">
               <VictoryChart
                 theme={VictoryTheme.material}
-                width={window.innerWidth*.4}
-                height={window.innerWidth*.4}
+                width={window.innerWidth * 0.4}
+                height={window.innerWidth * 0.4}
               >
-                <VictoryLine
-                  data={data}
-                  x="date"
-                  y="current"
-                  />
+                <VictoryLine data={data} x="date" y="current" />
               </VictoryChart>
             </div>
           </div>
         ) : (
           <div>
             <div className="App-spinner" />
-            <h3>Loading data...</h3>
+            <h3>
+              Loading data
+              <span className="App-dot-1">.</span>
+              <span className="App-dot-2">.</span>
+              <span className="App-dot-3">.</span>
+            </h3>
           </div>
-        ) }
+        )}
       </main>
     </div>
   );
---
+++ src/DataSelector.tsx
@@ -0,0 +1,77 @@
+import React, {useState, useEffect, ChangeEvent, useCallback, Dispatch, SetStateAction} from 'react';
+
+import { SensorData, getDataRange, getDataStream } from './telemetry';
+
+import './DataSelector.css';
+
+export interface DataSelectorProps {
+  setData: Dispatch<SetStateAction<SensorData[]>>;
+}
+
+type DataSource = 'Live' | 'Range';
+
+function DataSelector({ setData }: DataSelectorProps) {
+  const [dataSource, setDataSource] = useState<DataSource>("Live");
+  const [startTime, setStartTime] = useState(new Date(2019, 8, 22));
+  const [endTime, setEndTime] = useState(new Date());
+
+  const handleOptionChange = useCallback((ev: ChangeEvent) => {
+    setDataSource((ev.target as HTMLInputElement).value as DataSource);
+  }, []);
+
+  useEffect(() => {
+    setData([]);
+    switch (dataSource) {
+      case "Live":
+        const sub = getDataStream().subscribe(newData => {
+          setData(d => [...d, newData]);
+        });
+        return () => sub.unsubscribe();
+      case "Range":
+        getDataRange(startTime, endTime).then(setData);
+        return;
+    }
+  }, [dataSource, startTime, endTime, setData]);
+
+  return (
+    <div className="DataSelector">
+      <div>
+        Data Source:
+        <label>
+          <input
+            type="radio"
+            value="Live"
+            checked={dataSource === "Live"}
+            onChange={handleOptionChange}
+          />
+          Live
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Range"
+            checked={dataSource === "Range"}
+            onChange={handleOptionChange}
+          />
+          Range
+        </label>
+      </div>
+      {dataSource === "Range" ? (
+        <div>
+          <input
+            type="datetime-local"
+            value={startTime.toISOString().slice(0, -1)}
+            onChange={ev => setStartTime(new Date(ev.target.value))}
+          />
+          <input
+            type="datetime-local"
+            value={endTime.toISOString().slice(0, -1)}
+            onChange={ev => setEndTime(new Date(ev.target.value))}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default DataSelector;
